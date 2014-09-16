bd7bde7fa5be95e8bc45f42ab8dd77d5617b454a
Brian Scaturro <scaturrob@gmail.com>
2014-09-16 10:51:25 -0400

    user SuiteLoader in run.php

--- run.php
+++ run.php
@@ -4,13 +4,14 @@ require_once 'bootstrap.php';
 
 use Peridot\Core\SpecResult;
 use Peridot\Runner\Runner;
+use Peridot\Runner\SuiteLoader;
 
-$specs = __DIR__ . '/specs/';
-$files = ['spec.spec.php', 'suite.spec.php', 'runner.spec.php', 'suiteloader.spec.php'];
+$loader = new SuiteLoader();
+$specs = $loader->load(__DIR__ . '/specs/');
 $result = new SpecResult();
 $runner = new Runner($result);
-foreach ($files as $file) {
-    $runner->runSpec($specs . $file);
+foreach ($specs as $file) {
+    $runner->runSpec($file);
 }
 $result = $runner->getResult();
 print $result->getSummary() . "\n";
