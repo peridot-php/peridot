e2d9ff3e5a4fe396cb247d04963b0a1f8c77caf2
Brian Scaturro <scaturrob@gmail.com>
2014-09-15 21:11:28 -0400

    update bootstrap to include assert exceptions

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,12 @@
 <?php
-
 use Peridot\Runner\SuiteFactory;
 use Peridot\Core\Suite;
 use Peridot\Core\Spec;
 
+/**
+ * @param $description
+ * @param callable $fn
+ */
 function describe($description, callable $fn) {
     $singleton = SuiteFactory::getInstance();
     $suite = new Suite();
@@ -11,9 +14,21 @@ function describe($description, callable $fn) {
     $fn();
 }
 
+/**
+ * @param $description
+ * @param $fn
+ */
 function it($description, $fn) {
     $singleton = SuiteFactory::getInstance();
     $suite = $singleton->getSuite();
     $spec = new Spec($description, $fn);
     $suite->addSpec($spec);
 }
+
+/**
+ * Change default assert behavior to throw exceptions
+ */
+assert_options(ASSERT_WARNING, false);
+assert_options(ASSERT_CALLBACK, function($script, $line, $message) {
+    throw new Exception($message);
+});
