902ded5276eae5214e669408b024d487a932bee9
Brian Scaturro <scaturrob@gmail.com>
2014-09-21 11:20:40 -0400

    use symfony console

---
+++ src/Console/Application.php
@@ -0,0 +1,54 @@
+<?php
+namespace Peridot\Console;
+
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Application
+ * @package Peridot\Console
+ */
+class Application extends ConsoleApplication
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(Version::NAME, Version::NUMBER);
+    }
+
+    /**
+     * Run the peridot application
+     */
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $this->add(new Command());
+        return parent::doRun($input, $output);
+    }
+
+    /**
+     * The default InputDefinition for the application. Leave it to specific
+     * Tester objects for specifying further definitions
+     *
+     * @return InputDefinition
+     */
+    public function getDefinition()
+    {
+        return new InputDefinition(array(
+            new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display this help message.')
+        ));
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    public function getCommandName(InputInterface $input)
+    {
+        return 'peridot';
+    }
+} 
---
+++ src/Console/Command.php
@@ -0,0 +1,55 @@
+<?php
+namespace Peridot\Console;
+
+use Peridot\Core\SpecResult;
+use Peridot\Runner\Context;
+use Peridot\Runner\Runner;
+use Peridot\Runner\SuiteLoader;
+use Symfony\Component\Console\Command\Command as ConsoleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Command
+ * @package Peridot\Console
+ */
+class Command extends ConsoleCommand
+{
+    public function __construct()
+    {
+        parent::__construct('peridot');
+    }
+
+    /**
+     * Configure the command
+     */
+    protected function configure()
+    {
+        $this->
+            addArgument('path', InputArgument::REQUIRED, 'The path to a directory or file containing tests.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $path = $input->getArgument('path');
+        $result = new SpecResult();
+
+        $loader = new SuiteLoader();
+        $loader->load($path);
+        $runner = new Runner(Context::getInstance()->getCurrentSuite());
+        $runner->run($result);
+        $output->writeln($result->getSummary());
+
+        if ($result->getFailureCount() > 0) {
+            return 1;
+        }
+        return 0;
+    }
+} 
