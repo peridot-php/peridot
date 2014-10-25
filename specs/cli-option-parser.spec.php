5d6823ad925a594da8fe38a6724216d87565e25d
Brian Scaturro <scaturrob@gmail.com>
2014-10-25 14:49:25 -0400

    CliOptionParser

---
+++ specs/cli-option-parser.spec.php
@@ -0,0 +1,20 @@
+<?php
+use Peridot\Console\CliOptionParser;
+
+describe('CliOptionParser', function() {
+    beforeEach(function() {
+        $this->search = ['-c', '--configuration'];
+        $this->args = ["test.php", "-c", "peridot.php", "another", "thing", '--configuration', 'file.php'];
+        $this->parser = new CliOptionParser($this->search, $this->args);
+    });
+
+    describe('->parse()', function() {
+        it('should return values associated with search', function() {
+            $values = $this->parser->parse();
+            $c = $values['c'];
+            $configuration = $values['configuration'];
+            assert($c == 'peridot.php', "expected c to equal peridot.php");
+            assert($configuration == "file.php", "expected configuration to equal file.php");
+        });
+    });
+});
---
+++ src/Console/CliOptionParser.php
@@ -0,0 +1,63 @@
+<?php
+namespace Peridot\Console;
+
+/**
+ * The CliOptionParser parser searches an array of
+ * arguments for the specified options and returns their
+ * values.
+ *
+ * @package Peridot\Console
+ */
+class CliOptionParser
+{
+    /**
+     * CLI options to search for
+     *
+     * @var array
+     */
+    protected $search;
+
+    /**
+     * The CLI arguments to search against 
+     *
+     * @var array
+     */
+    protected $arguments;
+
+    /**
+     * @var array $search
+     * @var array $arguments
+     */
+    public function __construct(array $search, array $arguments)
+    {
+        $this->search = $search;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * Parse arguments to find any options specified
+     * in the search string.
+     *
+     * @return array $parsed
+     */
+    public function parse()
+    {
+        $parsed = [];
+        $count = count($this->arguments);
+        for ($i = 1; $i < $count; $i++) {
+            $previous = $this->arguments[$i - 1];
+            $arg = $this->arguments[$i];
+
+            $needle = array_reduce($this->search, function($result, $search) use ($previous) {
+                return ($previous == $search) ? $search : $result; 
+            });
+
+            if (!$needle) {
+                continue;
+            }
+
+            $parsed[str_replace('-', '', $previous)] = $arg;
+        }
+        return $parsed;
+    }
+}
