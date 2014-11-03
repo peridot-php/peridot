14b799b886262fa0eb59da77e6ccc9791347d856
Brian Scaturro <scaturrob@gmail.com>
2014-11-02 22:03:37 -0500

    update fixture to expose problem

--- fixtures/samplespec.php
+++ fixtures/samplespec.php
@@ -10,9 +10,25 @@ describe("Spec", function() {
     it("should have a passing spec", function() {
     });
 
-    it("should have a failing spec", function() {
+    xit("should have a failing spec", function() {
         throw new Exception("failure");
     });
 
     it("should be pending");
+
+    beforeEach(function() {
+       $this->thing = new ArrayObject();
+    });
+
+    describe('A nested suite', function() {
+
+        beforeEach(function() {
+            $this->thing->append('hello');
+        });
+
+        it('should have access to thing', function() {
+            assert($this->thing instanceof ArrayObject, "should be array object");
+            assert($this->thing[0] == "hello", 'hello should be a thing');
+        });
+    });
 });
