db1047254fa9f4fc33c5379e75396d58f17e8764
Brian Scaturro <scaturrob@gmail.com>
2014-11-06 22:11:36 -0500

    use ansi sequence in colors and set tty env variable

--- specs/abstract-base-reporter.spec.php
+++ specs/abstract-base-reporter.spec.php
@@ -2,6 +2,7 @@
 use Evenement\EventEmitter;
 use Peridot\Configuration;
 use Peridot\Reporter\AbstractBaseReporter;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\NullOutput;
 
 describe('AbstractBaseReporter', function() {
@@ -35,7 +36,29 @@ describe('AbstractBaseReporter', function() {
 
             it ('should add escape sequences if ansicon is enabled', function() {
                 $colored = $this->reporter->color('success', 'good');
-                assert($colored == '<fg=green>good</fg=green>', "expected color with ansicon enabled");
+                assert($colored == "\033[32mgood\033[39m", "expected color with ansicon enabled");
+            });
+        });
+
+        context('when in a tty terminal', function() {
+
+            beforeEach(function() {
+                $this->reporter = new TtyTestReporter(new Configuration(), new ConsoleOutput(), new EventEmitter());
+            });
+
+            afterEach(function() {
+                putenv('PERIDOT_TTY=');
+            });
+
+            it('should set the PERIDOT_TTY environment variable', function() {
+                $this->reporter->color('success', 'text');
+                assert(getenv('PERIDOT_TTY') !== false, "peridot tty environment variable should have been set");
+            });
+
+            it('should write colors if the PERIDOT_TTY environment variable is present', function() {
+                putenv('PERIDOT_TTY=1');
+                $text = $this->reporter->color('success', 'text');
+                assert("\033[32mtext\033[39m" == $text, "colored text should have been written");
             });
         });
     });
@@ -51,5 +74,18 @@ class WindowsTestReporter extends AbstractBaseReporter
     protected function isOnWindows()
     {
         return true;
+    }
+}
+
+class TtyTestReporter extends AbstractBaseReporter
+{
+    public function init()
+    {
+
+    }
+
+    protected function isOnWindows()
+    {
+        return false;
     }
 }
