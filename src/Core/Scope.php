e419a08e24b094d3808d60fd004ec098fdad9e9a
Brian Scaturro <scaturrob@gmail.com>
2014-11-16 15:45:58 -0500

    update docs for core scope

--- src/Core/Scope.php
+++ src/Core/Scope.php
@@ -3,6 +3,12 @@ namespace Peridot\Core;
 
 use Peridot\Scope\Scope as PeridotScope;
 
+/**
+ * Extends PeridotScope and is maintained here to support
+ * backwards compatibility with plugins
+ *
+ * @package Peridot\Core
+ */
 class Scope extends PeridotScope
 {
 
