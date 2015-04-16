8c4e217dbc87ec64dd0fbaa28fea569051fd81b3
Brian Scaturro <scaturrob@gmail.com>
2015-04-16 15:47:59 -0400

    update travis build and doc generation

---
+++ specs/peridot.spec.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * A simple assertion to test modules are
+ * working together as intended
+ */
+describe('truth', function () {
+    it('should be true', function () {
+        assert(true);
+    });
+});
